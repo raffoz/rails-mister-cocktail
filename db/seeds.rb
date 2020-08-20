3821b9dcf337723c2cc506a93672502b0b384f77
Raffaele Viggiani <raffaele.viggiani@gmail.com>
2020-08-20 16:13:59 +0200

    created rest-client seed with 20 different ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning db"
+Ingredient.destroy_all
+puts "Cleaned db"
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+serialized_ingredients = RestClient.get(url)
+ingredients = JSON.parse(serialized_ingredients)
+
+count = 0
+
+until count == 20 do
+  ingredient = ingredients["drinks"].sample.values.first
+  ingredient = Ingredient.new(name: ingredient)
+
+  if ingredient.valid?
+    ingredient.save
+    puts ingredient.name
+    count += 1
+  else
+  end
+end
+
+puts "Done with 20 different ingredients :)"
